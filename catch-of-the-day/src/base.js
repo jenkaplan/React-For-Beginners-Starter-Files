bdb5611755916e1cd4f768d65c4c97c061f06b00
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2016-12-27 18:42:45 -0500

    this is after video #18

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyBVh659Uk-ayN10LX8DjPR9lhWkWYOE4q4",
+    authDomain: "catch-of-the-day-jen-kaplan.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-jen-kaplan.firebaseio.com"
+  });
+
+
+export default base;
