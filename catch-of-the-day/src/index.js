2fecf7e80e34ea938c22a1aeb92583ca2b88d7cf
Jennifer Kaplan <jenniferkaplan.nyc@gmail.com>
2017-01-01 18:05:17 -0500

    second commit

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
+  import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Match, Miss } from 'react-router';
 
 import './css/style.css';
 import App from './components/App';
-import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
+const repo =`/${window.location.pathname.split('/')[1]}`;
 const Root = () => {
   return (
-      <BrowserRouter>
+      <BrowserRouter basename={repo}>
         <div>
           <Match exactly pattern="/" component={StorePicker}/>
           <Match pattern="/store/:storeId" component={App}/>
